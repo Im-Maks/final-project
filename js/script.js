45d7b6ab375369f0b57e681251b0fdb11eafee8f
Abitov Maksud <maksqwerty012@gmail.com>
2023-01-05 02:25:46 +0400

    last project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $("#owl-example").owlCarousel({
+      items :3,
+      navigation: true,
+      
+      pagination: true,
+      responsive :true,
+     autoPlay: true,
+    });
+  });
+
+  
