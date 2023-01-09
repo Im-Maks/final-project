f57771cd58e617a99b11e52175c85a6250d75c68
Abitov Maksud <maksqwerty012@gmail.com>
2023-01-09 05:00:08 +0400

    fourth commit

--- js/script.js
+++ js/script.js
@@ -10,4 +10,40 @@ $(document).ready(function() {
     });
   });
 
+var Portfolio = {
+  sort: function(items) {
+      items.show();
+      $('#portfolio-content').find('div.portfolio-item').not(items).fadeOut(500);
+  },
+  showAll: function(items) {
+      items.fadeIn(500);
+  },
+  doSort: function() {
+      $('a', '#portfolio-sort').on('click', function() {
+          var $a = $(this);
+          if (!$a.is('#all')) {
+              var items = $('div[data-cat=' + $a.data('cat') + ']', '#portfolio-content');
+              Portfolio.sort(items);
+          } else {
+              Portfolio.showAll($('div.portfolio-item', '#portfolio-content'));
+          }
+      });
+  }
+};
+
+Portfolio.doSort();
+
+var all = document.getElementById("all")
+var brand = document.getElementById("brand")
+var work = document.getElementById("work")
+var web = document.getElementById("web")
+
+function myFunction() {
+  all.classList.remove("active");
+  brand.classList.remove("active");
+  work.classList.remove("active");
+  web.classList.remove("active");
   
+  
+
+}
